dc25d179c532d5530aece953e6ce88587f965366
anzudesuka <132572759+anzudesuka@users.noreply.github.com>
2023-05-04 14:52:28 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #sync rom
-repo init --depth=1 --no-repo-verify -u https://github.com/Project-Xtended/manifest.git -b xt -g default,-mips,-darwin,-notdefault
-git clone https://github.com/NFS-Project/local_manifest --depth 1 -b rosy-Xtended-13 .repo/local_manifests
+repo init --depth=1 --no-repo-verify -u https://github.com/DerpFest-AOSP/manifest.git -b 13 -g default,-mips,-darwin,-notdefault
+git clone https://github.com/mizuswork/local_manifests --depth 1 -b main .repo/local_manifests
 repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j8
 
 # build rom
@@ -9,16 +9,16 @@ timeStart
 
 source build/envsetup.sh
 export TZ=Asia/Jakarta
-export KBUILD_BUILD_USER=rosy
-export KBUILD_BUILD_HOST=nfsproject
-export BUILD_USERNAME=rosy
-export BUILD_HOSTNAME=nfsproject
-lunch xtended_rosy-userdebug
+export KBUILD_BUILD_USER=Mizushudesuyo
+export KBUILD_BUILD_HOST=weeb
+export BUILD_USERNAME=Mizushudesuyo
+export BUILD_HOSTNAME=weeb
+lunch derp_lavender-user
 mkfifo reading # Jangan di Hapus
 tee "${BUILDLOG}" < reading & # Jangan di Hapus
 build_message "Building Started" # Jangan di Hapus
 progress & # Jangan di Hapus
-timeout 95m make xtended -j8 > reading # Jangan di hapus text line (> reading)
+timeout 95m mka derp -j8 > reading # Jangan di hapus text line (> reading)
 
 retVal=$?
 timeEnd
